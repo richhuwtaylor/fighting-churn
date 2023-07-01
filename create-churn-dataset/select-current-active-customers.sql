df66a59534916fdbc4b086a20b2a71e7b1b645ca
Rich Taylor <rich@taylor.it>
2023-07-01 16:25:58 +0100

    Add notebook for producing cohort analysis plots for monthly metrics

--- create-churn-dataset/select-current-active-customers.sql
+++ create-churn-dataset/select-current-active-customers.sql
@@ -9,15 +9,15 @@ WITH metric_date AS
     FROM socialnet7.metric
 )
 SELECT m.account_id, d.last_metric_time,
-sum(CASE WHEN metric_name_id=0 THEN metric_value ELSE 0 END) AS like_per_month,
-sum(CASE WHEN metric_name_id=1 THEN metric_value ELSE 0 END) AS newfriend_per_month,
-sum(CASE WHEN metric_name_id=2 THEN metric_value ELSE 0 END) AS post_per_month,
-sum(CASE WHEN metric_name_id=3 THEN metric_value ELSE 0 END) AS adview_feed_per_month,
-sum(CASE WHEN metric_name_id=4 THEN metric_value ELSE 0 END) AS dislike_per_month,
-sum(CASE WHEN metric_name_id=5 THEN metric_value ELSE 0 END) AS unfriend_per_month,
-sum(CASE WHEN metric_name_id=6 THEN metric_value ELSE 0 END) AS message_per_month,
-sum(CASE WHEN metric_name_id=7 THEN metric_value ELSE 0 END) AS reply_per_month,
-sum(CASE WHEN metric_name_id=8 THEN metric_value ELSE 0 END) AS account_tenure
+    sum(CASE WHEN metric_name_id=0 THEN metric_value ELSE 0 END) AS like_per_month,
+    sum(CASE WHEN metric_name_id=1 THEN metric_value ELSE 0 END) AS newfriend_per_month,
+    sum(CASE WHEN metric_name_id=2 THEN metric_value ELSE 0 END) AS post_per_month,
+    sum(CASE WHEN metric_name_id=3 THEN metric_value ELSE 0 END) AS adview_feed_per_month,
+    sum(CASE WHEN metric_name_id=4 THEN metric_value ELSE 0 END) AS dislike_per_month,
+    sum(CASE WHEN metric_name_id=5 THEN metric_value ELSE 0 END) AS unfriend_per_month,
+    sum(CASE WHEN metric_name_id=6 THEN metric_value ELSE 0 END) AS message_per_month,
+    sum(CASE WHEN metric_name_id=7 THEN metric_value ELSE 0 END) AS reply_per_month,
+    sum(CASE WHEN metric_name_id=8 THEN metric_value ELSE 0 END) AS account_tenure
 FROM socialnet7.metric m 
 INNER JOIN metric_date d 
 ON m.metric_time = d.last_metric_time
